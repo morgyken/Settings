daef447b9ef5e008e749395ecbe61bbd1698335b
Samuel Odhiambo <sodhiambo@collabmed.com>
2016-10-24 16:16:42 +0300

    Add get schemes

--- Http/Controllers/ApiController.php
+++ Http/Controllers/ApiController.php
@@ -16,4 +16,8 @@ use Illuminate\Routing\Controller;
 
 class ApiController extends Controller {
 
+    public function get_schemes(\Illuminate\Http\Request $request) {
+        return get_schemes($request->id);
+    }
+
 }
