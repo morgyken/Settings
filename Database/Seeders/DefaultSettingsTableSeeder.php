a4ae2e6fff15ba4c16fedf0564bc4f46f8dc7fcc
Samuel Odhiambo <sodhiambo@collabmed.com>
2016-11-03 13:43:21 +0300

    Update seeder for settings

--- Database/Seeders/DefaultSettingsTableSeeder.php
+++ Database/Seeders/DefaultSettingsTableSeeder.php
@@ -2,28 +2,32 @@
 
 namespace Ignite\Settings\Database\Seeders;
 
+use Ignite\Settings\Entities\Settings;
 use Illuminate\Database\Seeder;
 
-class DefaultSettingsTableSeeder extends Seeder {
+class DefaultSettingsTableSeeder extends Seeder
+{
 
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run() {
+    public function run()
+    {
 
         /* `platform`.`settings_settings` */
         $settings_settings = [
             ['name' => 'core::site-name', 'value' => 'Collabmed Platform'],
             ['name' => 'core::site-description', 'value' => 'An elegant , highly opinionated modular healthcare system'],
+            ['name' => 'core::real-time', 'value' => true],
             ['name' => 'evaluation::receipt_prefix', 'value' => 'COL'],
             ['name' => 'reception::checkin_destinations', 'value' => '["1","5","9"]'],
-            ['name' => 'reception::checkin_places', 'value' => '["laboratory","theatre","diagnostics","radiology"]'],
-            ['name' => 'settings::app_name', 'value' => 'Collabmed Platform']
+            ['name' => 'reception::checkin_places', 'value' => '["laboratory","theatre","diagnostics","radiology","pharmacy"]']
         ];
-        foreach ($settings_settings as $setting)
-            \Ignite\Settings\Entities\Settings::create($setting);
+        foreach ($settings_settings as $setting) {
+            Settings::create($setting);
+        }
     }
 
 }
