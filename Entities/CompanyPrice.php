0c8b65e0df2559fca1b08be03cca40efb02773bd
Samuel Dervis <samueldervis@gmail.com>
2017-06-15 20:51:22 +0300

    Updates phpdoc

--- Entities/CompanyPrice.php
+++ Entities/CompanyPrice.php
@@ -4,6 +4,25 @@ namespace Ignite\Settings\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Ignite\Settings\Entities\CompanyPrice
+ *
+ * @property int $id
+ * @property int $procedure
+ * @property int $company
+ * @property float $price
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \Ignite\Settings\Entities\Insurance $firm
+ * @property-read \Ignite\Evaluation\Entities\Procedures $procedures
+ * @method static \Illuminate\Database\Query\Builder|\Ignite\Settings\Entities\CompanyPrice whereCompany($value)
+ * @method static \Illuminate\Database\Query\Builder|\Ignite\Settings\Entities\CompanyPrice whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Ignite\Settings\Entities\CompanyPrice whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Ignite\Settings\Entities\CompanyPrice wherePrice($value)
+ * @method static \Illuminate\Database\Query\Builder|\Ignite\Settings\Entities\CompanyPrice whereProcedure($value)
+ * @method static \Illuminate\Database\Query\Builder|\Ignite\Settings\Entities\CompanyPrice whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class CompanyPrice extends Model {
 
     protected $fillable = [];
